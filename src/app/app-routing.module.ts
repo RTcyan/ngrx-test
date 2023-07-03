be9192d5e7dc4a1c70e513e9108f19774872077c
Maxim <mgudkov@digital-spirit.ru>
2023-07-03 16:17:23 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'info',
+    pathMatch: 'prefix',
+  },
+  {
+    path: 'info',
+    component: MainLayoutComponent,
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
