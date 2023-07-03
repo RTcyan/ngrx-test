ad45cf87abe469bebc49ee04a4321dae3861ea62
Maxim <mgudkov@digital-spirit.ru>
2023-07-03 16:46:41 +0300

    change footer

--- src/app/layout/main-layout/footer/footer.component.ts
+++ src/app/layout/main-layout/footer/footer.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class FooterComponent {
+  public get year(): number {
+    return new Date().getFullYear();
   }
-
 }
