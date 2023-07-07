6860765e4151d9fffa50aa53b55aaf8f229baf0a
Maxim <mgudkov@digital-spirit.ru>
2023-07-07 14:01:50 +0300

    add state to main info component

--- src/app/info/user/full-info/main/main.component.ts
+++ src/app/info/user/full-info/main/main.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { TuiStringHandler } from '@taiga-ui/cdk';
-import { FormService, MAIN_INFO_FROM_GROUP } from '../../services/form.service';
-import { Gender } from '../../models/gender';
+import { Gender } from '../../../../api/info/models/user/gender';
+import { Store } from '@ngrx/store';
+import { UserActions, UserSelectors } from 'src/app/info/state';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit, OnDestroy {
+  public destroy$ = new Subject<void>();
+
+  public form = new FormGroup({
+    name: new FormControl(),
+    surname: new FormControl(),
+    gender: new FormControl(),
+  });
+  
   public genders: readonly Gender[] = [
     {
       code: 1,
@@ -23,11 +34,24 @@ export class MainComponent {
 
   public genderStringify: TuiStringHandler<Gender> = gender => gender.value;
 
-  public constructor(private formService: FormService) {
-    console.log(this.formService);
+  public constructor(private store: Store) {}
+
+  public ngOnInit(): void {
+    this.store.select(UserSelectors.mainInfoSelector).pipe(
+      takeUntil(this.destroy$),
+    ).subscribe((mainInfo) => {
+      this.form.patchValue(mainInfo, { emitEvent: false });
+    });
+
+    this.form.valueChanges.pipe(
+      takeUntil(this.destroy$),
+    ).subscribe((mainInfo) => {
+      this.store.dispatch(UserActions.mainInfoChange({ mainInfo }))
+    });
   }
 
-  public get form(): FormGroup {
-    return this.formService.form.controls[MAIN_INFO_FROM_GROUP] as FormGroup;
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
